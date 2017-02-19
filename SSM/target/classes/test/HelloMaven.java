6dcce2686350f9df2a0afd0d93ae9cadb817080b
liaodingyuan <634748078@qq.com>
2017-02-20 01:52:30 +0800

    新建maven web工程

---
+++ SSM/target/classes/test/HelloMaven.java
@@ -0,0 +1,15 @@
+package main.resources.test;
+
+/**
+ * Created by Administrator on 2017/2/20.
+ */
+public class HelloMaven {
+
+    public HelloMaven(){
+
+    }
+    public static void main(String[] args){
+
+        System.out.println("Hello Maven!");
+    }
+}
